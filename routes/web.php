bac9dec0cf9df1a663154b9c21eacb29bdb5446a
Eduardo Marçal <edumarcalnet@gmail.com>
2018-06-12 06:08:19 -0300

    [#12] - Implementa o token da sessão do usuário.

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+$this->group(['middleware' => ['auth', 'auth.unique.user']], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
